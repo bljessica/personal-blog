1fdbecd9b781437910bffe79446fb59dd8292cf5
bljessica <1194620082@qq.com>
2020-09-17 11:30:39 +0800

    fix header

--- src/components/nav-header.vue
+++ src/components/nav-header.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="header">
-        <div class="title">
-            <a href="">
-                <i class="iconfont icon-custom-logo"></i>
-                <span>{{ title }}</span>
-            </a>
-        </div>
-        <ul class="nav">
-            <li v-for="(item, index) in navItems" :key="index">
+        <div class="header-container">
+            <div class="title">
                 <a href="">
-                    <i class="iconfont" :class="icons[index]"></i>
-                    {{ item }}
+                    <i class="iconfont icon-custom-logo"></i>
+                    <span>{{ title }}</span>
                 </a>
-                <img v-if="index == 2 || index == 3" src="../assets/down.png" alt="">
-            </li>
-            <li><i class="iconfont icon-sousuo search"></i></li>
-        </ul>
+            </div>
+            <ul class="nav">
+                <li v-for="(item, index) in navItems" :key="index">
+                    <a href="">
+                        <i class="iconfont" :class="icons[index]"></i>
+                        {{ item }}
+                    </a>
+                    <img v-if="index == 2 || index == 3" src="../assets/down.png" alt="">
+                </li>
+                <li><i class="iconfont icon-sousuo search"></i></li>
+            </ul>
+        </div>
         <h1 class="slogan">Learn a little every day</h1>
     </div>
 </template>
@@ -55,61 +57,68 @@ export default {
             text-decoration: none;
             color: white;
         }
-        .title {
-            position: absolute;
-            left: 150px;
+        .header-container {
             height: 60px;
-            width: 250px;
-            line-height: 60px;
-            font-size: 30px;
-            i {
-                display: inline-block;
-                font-size: 34px;
-                margin-right: 10px;
-            }
-        }
-        .nav {
-            list-style-type: none;
-            position: absolute;
-            right: 150px;
-            width: 600px;
-            li {
-                display: inline-block;
-                text-align: center;
-                line-height: 60px;
+            width: 100%;
+            position: fixed;
+            background: #1DA7DA;
+            z-index: 1001;
+            .title {
+                position: absolute;
+                left: 150px;
                 height: 60px;
-                font-size: 16px;
-                padding: 0 15px 0 12px;
-                position: relative;
-                cursor: pointer;
+                width: 250px;
+                line-height: 60px;
+                font-size: 30px;
                 i {
                     display: inline-block;
-                    font-size: 18px;
-                    position: relative;
-                    left: 2px;
-                    top: 1px;
-                }
-                img {
-                    width: 16px;
-                    height: 16px;
-                    position: relative;
-                    top: 3px;
-                    margin-left: 3px;
-                }
-                .search {
-                    display: inline-block;
-                    font-size: 22px;
-                    color: white;
-                    position: relative;
-                    top: 2px;
+                    font-size: 34px;
+                    margin-right: 10px;
                 }
             }
-            li:hover {
-                background: rgba(0, 0, 0, 0.1);
-                transition: .5s;
-                img {
-                    transform: rotate(180deg);
+            .nav {
+                list-style-type: none;
+                position: absolute;
+                right: 150px;
+                width: 600px;
+                li {
+                    display: inline-block;
+                    text-align: center;
+                    line-height: 60px;
+                    height: 60px;
+                    font-size: 16px;
+                    padding: 0 15px 0 12px;
+                    position: relative;
+                    cursor: pointer;
+                    i {
+                        display: inline-block;
+                        font-size: 18px;
+                        position: relative;
+                        left: 2px;
+                        top: 1px;
+                    }
+                    img {
+                        width: 16px;
+                        height: 16px;
+                        position: relative;
+                        top: 3px;
+                        margin-left: 3px;
+                    }
+                    .search {
+                        display: inline-block;
+                        font-size: 22px;
+                        color: white;
+                        position: relative;
+                        top: 2px;
+                    }
+                }
+                li:hover {
+                    background: rgba(0, 0, 0, 0.1);
                     transition: .5s;
+                    img {
+                        transform: rotate(180deg);
+                        transition: .5s;
+                    }
                 }
             }
         }
